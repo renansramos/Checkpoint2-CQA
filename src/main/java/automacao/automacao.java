8dbe6a7234a7ac14d688c8e25aad1cbe23c85cb4
Renan Ramos <renan_dasilvaramos@hotmail.com>
2022-05-22 22:46:56 -0300

    inicial

---
+++ src/main/java/automacao/automacao.java
@@ -0,0 +1,21 @@
+package automacao;
+import org.openqa.selenium.By;
+
+import java.util.List;
+
+import org.openqa.selenium.*;
+
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+public class automacao {
+	public static void main(String[] args) {
+		
+		
+
+    }
+}
