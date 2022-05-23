1c55ee2414c8bd7ea5139d3a5f294408031b72d0
Renan Ramos <renan_dasilvaramos@hotmail.com>
2022-05-22 22:57:18 -0300

    acessando a pagina de teste 1

--- src/main/java/automacao/automacao.java
+++ src/main/java/automacao/automacao.java
@@ -17,16 +17,21 @@ public class automacao {
 		
 		
 		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-		
-		
+	
+	
 		WebDriver driver = new ChromeDriver();
-		
-		driver.get("https://wiki.ixcsoft.com.br/index.php/IXCwiki:P%C3%A1gina_de_testes");
-	    System.out.println("O nome da página acessada é: " + driver.getTitle());
-	               
+	
+	 	driver.get("https://wiki.ixcsoft.com.br/index.php/IXCwiki:P%C3%A1gina_de_testes");
+        System.out.println("O nome da página acessada é: " + driver.getTitle());
+               
+        WebElement btn = driver.findElement(By.xpath("//*[@id=\"mw-content-text\"]/div/div[3]/table/tbody/tr[1]/td[1]/a[2]"));
+        btn.click();
+        System.out.println("CLK1 - Entra na pagina de testes 1: " + driver.getTitle());
 	        
-
-       
+	       
+	    
+	        
+ 
 
     }
 }
